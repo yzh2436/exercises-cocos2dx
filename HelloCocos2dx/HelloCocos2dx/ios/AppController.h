ba946d0c4060d48536a19166f7f0251777bde7e5
G_will <g_will@ieqi.com>
2012-11-27 14:34:31 +0800

    init

---
+++ HelloCocos2dx/HelloCocos2dx/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  HelloCocos2dxAppController.h
+//  HelloCocos2dx
+//
+//  Created by johnny on 12-11-27.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ HelloCocos2dx/HelloCocos2dx/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloCocos2dxAppController.h
+//  HelloCocos2dx
+//
+//  Created by johnny on 12-11-27.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
